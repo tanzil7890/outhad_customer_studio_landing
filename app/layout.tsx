8f06126a7f2ad1701a9ef49b46d55301df25f076
Mohammad Tanzil Idrisi <tanzilidrisi@gmail.com>
2026-04-08 15:53:11 -0500

    fix(analytics): update GTM container ID to GTM-MGZL5X3R

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,7 +120,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M2W73R5V');`,
+})(window,document,'script','dataLayer','GTM-MGZL5X3R');`,
           }}
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -145,7 +145,7 @@ html {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M2W73R5V"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MGZL5X3R"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
