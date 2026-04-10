02c5a6931e0f7ec4f581d5adaf3f343e02b15a23
Mohammad Tanzil Idrisi <tanzilidrisi@gmail.com>
2026-04-09 23:22:43 -0500

    script clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -194,6 +194,21 @@ html {
           {children}
         </main>
 
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w9bwc2wdi5");
+            `,
+          }}
+        />
+
         {/* LinkedIn Insight Tag */}
         <Script
           id="linkedin-insight"
